636cd80af20a6437a87af8a575a09abb717aa7f7
abdelrhaman werby <47545430+abwerby@users.noreply.github.com>
2020-04-05 03:02:34 +0200

    first commit..

---
+++ vehicle_model.cpp
@@ -0,0 +1,89 @@
+#include "vehicle_model.h"
+#include <cmath>
+#include <random>
+#include <iostream>
+
+using namespace std;
+
+#define RAD2DEG 57.2957795
+
+state::state(double _x, double _y, double _th, double _v, double _cte, double _eth)
+{
+	this->x = _x;
+	this->y = _y;
+	this->th = _th;
+	this->v = _v;
+	this->cte = _cte;
+	this->eth = _eth;
+}
+
+void state::print_state()
+{
+	cout << "X = "       << this->x << " Meter" << endl;
+	cout << "Y = "       << this->y << " Meter" << endl;
+	cout << "V = "       << this->v << " M/S" << endl;
+	cout << "Theta = "   << this->th  * RAD2DEG << " Degree" << endl;
+	cout << "E_theta = " << this->eth * RAD2DEG << " Degree" << endl;
+	cout << "CTE= "      << this->cte << " Meter" << endl;
+}
+
+
+inputs::inputs(double _steerangle, double _accelartion)
+{
+	this->steerangle = _steerangle;
+	this->accelartion = _accelartion;
+}
+
+void inputs::print_inputs()
+{
+	cout << "steer angle: " << this->steerangle;
+	cout << "\t accelration: " << this->accelartion << endl;
+}
+
+double polyval(double* p, int n, double x)
+{
+	double px = 0.0;
+
+	for (int i = 0; i < n; i++)
+	{
+		px += p[i] * pow(x, n - i - 1);
+	}
+
+	return px;
+}
+
+state update_state(inputs input, state last_state, double* coff, double dt, double base_length)
+{
+	state current_state;
+	double th_des;
+
+	current_state.x = last_state.x + last_state.v * cos(last_state.th) * dt;
+	current_state.y = last_state.y + last_state.v * sin(last_state.th) * dt;
+	current_state.th = last_state.th + (last_state.v / base_length) * input.steerangle * dt;
+	current_state.v = last_state.v + input.accelartion * dt;
+
+	th_des = atan(coff[2] + 2 * coff[1] * last_state.x + 3 * coff[0] * pow(last_state.x, 2));
+
+	current_state.cte = polyval(coff, 4, last_state.x) - last_state.y + (last_state.v * sin(last_state.eth) * dt);
+	current_state.eth = last_state.th - th_des + ((last_state.v / base_length) * input.steerangle * dt);
+
+	return current_state;
+}
+
+state update_state_noise(inputs input, state last_state, double* coff, double dt, double base_length)
+{
+	state current_state;
+	double th_des;
+
+	current_state.x = last_state.x + last_state.v * cos(last_state.th) * dt;
+	current_state.y = last_state.y + last_state.v * sin(last_state.th) * dt;
+	current_state.th = last_state.th + (last_state.v / base_length) * input.steerangle * dt + (((double)rand() / (RAND_MAX)) - 0.5) * 0.01;
+	current_state.v = last_state.v + input.accelartion * dt - ((double)rand() / (RAND_MAX)) * 0.05;
+
+	th_des = atan(coff[2] + 2 * coff[1] * last_state.x + 3 * coff[0] * pow(last_state.x, 2));
+
+	current_state.cte = polyval(coff, 4, last_state.x) - last_state.y + (last_state.v * sin(last_state.eth) * dt);
+	current_state.eth = last_state.th - th_des + ((last_state.v / base_length) * input.steerangle * dt);
+
+	return current_state;
+}
---
+++ vehicle_model.h
@@ -0,0 +1,84 @@
+#pragma once
+
+
+
+/**
+ * @Brief : class save the vehicle state and location
+ *
+ * @param x : the X (longtuide) postion of the vehicle in meter.
+ * @param y : the Y (letral) postion of the vehicle in meter.
+ * @param v : the linear velocity of the vehicle in meter per second.
+ * @param th  : the heading angle of the vehicle in radian.
+ * @param eth : the heading angle error to the ref trajectory of the vehicle in radian.
+ * @param cte : the letral location error to the ref trajectory of the vehicle in meter.
+ *
+ */
+class state
+{
+public:
+	double x;
+	double y;
+	double th;
+	double v;
+	double cte;
+	double eth;
+	state(double _x=0, double _y=0, double _th=0, double _v=0, double _cte=0, double eth=0);
+	void print_state();
+
+};
+
+
+/**
+ * @Brief : class save the vehicle inputs accelartion and steer angle
+ *
+ * @param steerangle  : the steer angle of the vehicle front wheel in radian (-0.43, 0.43).
+ * @param accelartion : the accelartion of the vehicle (-1, 1).
+ *
+ */
+class inputs
+{
+public:
+	double steerangle;
+	double accelartion;
+	inputs(double _steerangle=0, double _accelartion=0);
+	void print_inputs();
+};
+
+
+/**
+ * @Brief : evulate the f(x) of polynomial
+ *
+ * @param p : the coffencint array pointer.
+ * @param n : the coff array length.
+ * @param x : the input number to the polynomial.
+ *
+ * @Return : f(x) 
+ */
+double polyval(double* p, int n, double x);
+
+/**
+ * @Brief : function to the kinmatics bycicle model of vehicle.
+ *
+ * @param input : the inputs commmand to the vehicle.
+ * @param last_state : the last state of the vehicle.
+ * @param coff : the coff of the polynomial to ref trajectory.
+ * @param dt  : the sample time of system in seconds.
+ * @param base_length : the base length of the vehicle in meter.
+ *
+ * @Return : the new state of the vehicle
+ */
+state update_state(inputs input, state last_state, double* coff, double dt, double base_length);
+
+/**
+ * @Brief : function to the kinmatics bycicle model of vehicle with noise in heading angle and add friction to reduce velocity.
+ *
+ * @param input : the inputs commmand to the vehicle.
+ * @param last_state : the last state of the vehicle.
+ * @param coff : the coff of the polynomial to ref trajectory.
+ * @param dt  : the sample time of system in seconds.
+ * @param base_length : the base length of the vehicle in meter.
+ *
+ * @Return : the new state of the vehicle
+ */
+state update_state_noise(inputs input, state last_state, double* coff, double dt, double base_length);
+
